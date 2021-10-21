8edc500478349d29cebdd2a76cb81214813536db
Hattinger04 <s.8greiderer@gmail.com>
2021-10-21 08:56:05 +0200

    create article and shop list

--- FirstWebApp/Controllers/ShopController.cs
+++ FirstWebApp/Controllers/ShopController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FirstWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,11 +16,23 @@ namespace FirstWebApp.Controllers
 
         public IActionResult Shop()
         {
-            return View();
+            List <Article> articles = ArticleFromDB(); 
+            return View(articles);
         }
         public IActionResult Basket()
         {
             return View();
         }
+        private List<Article> ArticleFromDB()
+        {
+            return new List<Article>() {
+                new Article() { ArticleID = 1, ArticleName = "RaspberryPI", Brand = "PI", ReleaseDate= new DateTime(2021,10,5) },
+                new Article() { ArticleID = 2, ArticleName = "Handy", Brand = "Samsung", ReleaseDate= new DateTime(2014,11,8) },
+                new Article() { ArticleID = 3, ArticleName = "Maus ", Brand = "Razor", ReleaseDate= new DateTime(2017,5,10) },
+                new Article() { ArticleID = 4, ArticleName = "Uhr", Brand = "Rolex", ReleaseDate= new DateTime(1991,8,23) }
+            };
+        }
     }
+    // Ist nicht von DB, kann es aber geben
+    
 }
---
+++ FirstWebApp/Models/Article.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebApp.Models
+{
+    public class Article
+    {
+        private int articleID;
+
+        public int ArticleID
+        {
+            get { return this.articleID; }
+            set
+            {
+                if (value >= 0)
+                {
+                    this.articleID = value;
+                }
+            }
+        }
+        public string ArticleName { get; set; }
+        public string Brand { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
