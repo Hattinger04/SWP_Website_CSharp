d5c9707afe5cf02d3361e0d6bdea13b323a8fc1d
Hattinger04 <s.8greiderer@gmail.com>
2022-01-27 08:26:16 +0100

    adding database functions (not tested)

--- FirstWebApp/Models/DB/RepositoryUsersDB.cs
+++ FirstWebApp/Models/DB/RepositoryUsersDB.cs
@@ -26,8 +26,52 @@ namespace FirstWebApp.Models.DB {
             }
         }
 
-        public bool ChangeUserData(int userID, User newUserData) {
-            throw new NotImplementedException();
+        public bool ChangeUserData(int userID, User user) {
+            if (this.connection?.State == System.Data.ConnectionState.Open) {
+                DbCommand cmd = this.connection.CreateCommand();
+                cmd.CommandText = "update users set username = @username, password = sha2(@password, 512), " +
+                    "email = @email, birthdate = @birthdate, gender = @gender) where user_id = @user_id";
+                DbParameter paramUN = cmd.CreateParameter();
+                paramUN.ParameterName = "username";
+                paramUN.DbType = System.Data.DbType.String;
+                paramUN.Value = user.Username;
+
+                DbParameter paramPW = cmd.CreateParameter();
+                paramPW.ParameterName = "password";
+                paramPW.DbType = System.Data.DbType.String;
+                paramPW.Value = user.Password;
+
+                DbParameter paramEmail = cmd.CreateParameter();
+                paramEmail.ParameterName = "email";
+                paramEmail.DbType = System.Data.DbType.String;
+                paramEmail.Value = user.Email;
+
+                DbParameter paramBD = cmd.CreateParameter();
+                paramBD.ParameterName = "birthdate";
+                paramBD.DbType = System.Data.DbType.Date;
+                paramBD.Value = user.Birthdate;
+
+                DbParameter paramGender = cmd.CreateParameter();
+                paramGender.ParameterName = "gender";
+                paramGender.DbType = System.Data.DbType.Int32;
+                paramGender.Value = user.Gender;
+
+                DbParameter paramID = cmd.CreateParameter();
+                paramGender.ParameterName = "user_id";
+                paramGender.DbType = System.Data.DbType.Int32;
+                paramGender.Value = user.UserID;
+
+
+                cmd.Parameters.Add(paramUN);
+                cmd.Parameters.Add(paramPW);
+                cmd.Parameters.Add(paramEmail);
+                cmd.Parameters.Add(paramBD);
+                cmd.Parameters.Add(paramGender);
+                cmd.Parameters.Add(paramID);
+
+                return cmd.ExecuteNonQuery() == 1;
+            }
+            return false; 
         }
 
         public bool Delete(int user_id) {
@@ -64,8 +108,24 @@ namespace FirstWebApp.Models.DB {
             }
             return users; 
         }
-        public User GetUser() {
-            return null; 
+        public User GetUser(int user_id) {
+            if (this.connection?.State == System.Data.ConnectionState.Open) {
+                DbCommand cmd = this.connection.CreateCommand();
+                cmd.CommandText = "select * from users where user_id = @user_id";
+                using (DbDataReader reader = cmd.ExecuteReader()) {
+                    if(reader.Read()) {
+                        return new User() {
+                            UserID = user_id,
+                            Username = Convert.ToString(reader["username"]),
+                            Password = Convert.ToString(reader["password"]),
+                            Birthdate = Convert.ToDateTime(reader["birthdate"]),
+                            Email = Convert.ToString(reader["email"]),
+                            Gender = (Gender)Convert.ToInt32(reader["gender"])
+                        };
+                    }
+                }               
+            }
+            return new User(); 
         }
         public bool Insert(User user) {
             if (this.connection?.State == System.Data.ConnectionState.Open) {
